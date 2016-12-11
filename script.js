371a3a703d3b6e154269b4c68076886ac87be3e9
Yechiel K <chilikkal@gmail.com>
2016-12-11 15:04:42 -0500

    Done.

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $.get('sentence.html', function(response) {
+    $('#sentences').html(response);
+  })
+
+  $.get('bad_link.html', function(data) {
+    doSomething();
+  }).fail(function(error) {
+    console.log('Something went wrong: ' + error);
+  })
+})
+
+var url = "https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078";
+
+$.get(url).done(function(data) {
+  console.log("Done");
+  console.log(data);
+});
